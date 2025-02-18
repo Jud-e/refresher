ab560d1ea8c9f9aa104a022334b3f96d126e6b2c
Jude Enuanwa <lenuanwa@gmail.com>
2025-02-18 02:00:08 -0800

    rev1

--- exercises.js
+++ exercises.js
@@ -57,3 +57,45 @@ check.onclick = function(){
     days = age * 365;
     alert(`You are ${days} days old`);
 }
+
+//Circle area and circumference
+let radius;
+let area;
+let circumference;
+let result2;
+let circleCal = document.getElementById("calCircle");
+circleCal.onclick = function(){
+    radius = document.getElementById("radius");
+    radius = Number(radius.value);
+    area = Math.PI * Math.pow(radius, 2);
+    circumference = 2 * Math.PI * radius;
+    result2 = document.getElementById("result2");
+    result2.textContent = `The area of your circle is ${area} and the circumference is ${circumference}`
+}
+
+//Random number generator
+let counter = 1;
+let randomNum;
+let guess;
+let calculate;
+
+calculate = document.getElementById("calculate");
+calculate.onclick = function(){
+    if (counter <=3) {
+        guess = document.getElementById("guess");
+        guess = Number(guess.value);
+        console.log(guess);
+        randomNum = Math.floor(Math.random() * 10) + 1;
+        // randomNum = 3;
+        console.log(randomNum);
+        if (guess == randomNum) {
+            alert("You got it right");
+        }else{
+            alert("You got it wrong");
+        }
+        counter++;
+    }
+    else{
+        alert("You're out of tries");
+    }
+}
