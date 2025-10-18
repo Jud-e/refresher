90f08486296b97f9f00205d407a1ca0a4c116ace
Jude Enuanwa <lenuanwa@gmail.com>
2025-10-18 14:30:57 -0700

    help me

--- New folder/test/src/components/addStudent.jsx
+++ New folder/test/src/components/addStudent.jsx
@@ -1,8 +1,15 @@
 const AddStudent = (add = (f)=>f) => {
+    const [name, setName] = useState("");
+
+    const handleSubmit = () => {
+    if (!name.trim()) return;
+    add(name);
+    setName(""); // clear input
+  };
     return ( <>
         <label>Name: </label>
-        <input type="text" name="name" id="name" /><br /><br />
-        <input type="button" value="Submit" onClick={add}/>
+        <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)}/><br /><br />
+        <input type="button" value="Submit" onClick={handleSubmit}/>
     </> );
 }
  
